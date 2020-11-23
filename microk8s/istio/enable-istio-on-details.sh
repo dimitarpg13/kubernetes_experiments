aed6981aba210c05e0c97454e7d6e0df617476bc
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2020-11-22 21:08:40 -0500

    more scriptlets

---
+++ microk8s/istio/enable-istio-on-details.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/istio/istio/release-1.8/samples/bookinfo/platform/kube/bookinfo.yaml | microk8s istioctl kube-inject -f - | microk8s kubectl apply -l app!=reviews -f -
