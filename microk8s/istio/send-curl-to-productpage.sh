21d6b9c06add65a42fc506162891b04e1c971fbe
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2020-11-26 17:34:48 -0500

    more scriptlets

---
+++ microk8s/istio/send-curl-to-productpage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+k8s_host_ip=$(cat /etc/hosts | grep 'istio\.tutorial\.bookinfo\.com' | awk '{ print $1 }')
+if [[ -z $k8s_host_ip ]]; then
+ echo "Kubernetes host is not found in /etc/hosts! Exitting...";
+ exit -1 
+fi
+curl -HHost:istio.tutorial.bookinfo.com http://istio.tutorial.bookinfo.com:31380/productpage
