2ac6c9d827f45d9b62fe600d2cf30ccb6acc8b7e
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2020-12-03 13:24:29 -0500

    more scriptlets

--- microk8s/istio/send-infinite-get-to-productpage-from-remote.sh
+++ microk8s/istio/send-infinite-get-to-productpage-from-remote.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-k8s_host_ip=$(cat /etc/hosts | grep 'istio\.tutorial\.bookinfo\.com' | awk '{ print $1 }')
+if [[ -z $ROOT_HOSTS_FOLDER ]]; then
+  echo "ROOT_HOSTS_FOLDER is not set! Assuming '/' for root.."
+fi
+
+k8s_host_ip=$(cat $ROOT_HOSTS_FOLDER/etc/hosts | grep 'istio\.tutorial\.bookinfo\.com' | awk '{ print $1 }')
 if [[ -z $k8s_host_ip ]]; then
  echo "Kubernetes host is not found in /etc/hosts! Exitting...";
  exit -1
