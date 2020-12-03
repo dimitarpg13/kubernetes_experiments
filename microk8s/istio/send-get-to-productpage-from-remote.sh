46ccf5b440037ecbfab2169f1410b9a33b0a929b
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2020-12-03 13:23:52 -0500

    more scriptlets

--- microk8s/istio/send-get-to-productpage-from-remote.sh
+++ microk8s/istio/send-get-to-productpage-from-remote.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+if [[ -z $ROOT_HOSTS_FOLDER ]]; then
+  echo "ROOT_HOSTS_FOLDER is not set! Assuming '/' for root.."
+fi
+
 k8s_host_ip=$(cat /etc/hosts | grep 'istio\.tutorial\.bookinfo\.com' | awk '{ print $1 }')
 if [[ -z $k8s_host_ip ]]; then
  echo "Kubernetes host is not found in /etc/hosts! Exitting...";
  exit -1 
 fi
+
 curl -HHost:istio.tutorial.bookinfo.com http://istio.tutorial.bookinfo.com:31380/productpage
